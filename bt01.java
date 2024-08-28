091085536168a1ad1918643198535a62718f028c
haiyen2304 <hhyhoanhaiyen2304@gmail.com>
2024-08-28 23:20:09 +0700

    e nop bt

---
+++ bt01.java
@@ -0,0 +1,21 @@
+package btsession12;
+
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class bt01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<String> stack = new Stack<>();
+        System.out.println("Nhap so phan tu vao trong mang");
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < n; i++) {
+            System.out.println("Nhap phan tu vao trong mang "+(i+1));
+             stack.push(sc.nextLine());
+        }
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
